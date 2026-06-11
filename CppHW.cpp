ac85e9c2cc26950b1e46acc8d94ee29d6bf11d96
TheFlw <00001214883060sp@al.educacao.sp.gov.br>
2026-06-11 07:22:58 -0300

    Update CppHW.cpp

--- CppHW.cpp
+++ CppHW.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 int main(){
-  std::cout << "Hello, World";
-  return 0;
+  std::cout << " Welcome to Hello World experence" << std::endl;
+  std::cout << "Hello, World" << std::endl;
+  return 0;  //the exit code
 }
